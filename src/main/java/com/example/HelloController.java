c278b33f14a92cfe374bf46c4b9c149c62f23af7
Hillary Skye <skyeinc9@gmail.com>
2017-02-18 13:56:24 -0700

    Added @DeleteMapping and @PatchMapping

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,18 @@ public class HelloController {
         return "Hello from Spring!";
     }
 
+    @GetMapping("/tasks")
+    public String addWorld() {
+        return "add from Spring";
+    }
+
+    @DeleteMapping("/tasks")
+    public String deleteWorld() {
+        return "Deleted from Spring";
+    }
+
+    @PatchMapping("/hillary")
+    public String hillaryWorld() {
+        return "Patch from Spring";
+    }
 }
